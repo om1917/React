78b217bd3f5be2c53252c400de13ee0d0c186815
DESKTOP-HD94HJ1\Manoj <om.bhaisora@gmail.com>
2023-09-10 15:27:11 +0530

    changes

--- Components/viewList.js
+++ Components/viewList.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements';
 
 
+const data = [
+    { id: '1', name: 'John', age: '30', country: 'USA' },
+    { id: '2', name: 'Alice', age: '25', country: 'Canada' },
+    { id: '3', name: 'Bob', age: '28', country: 'UK' },
+    // Add more data as needed
+  ];
 
 const ViewList = () => {
-    // Sample data for your data table
-    const tableData = [
-      ['Name', 'Age', 'Country'],
-      ['John', '30', 'USA'],
-      ['Alice', '25', 'Canada'],
-      ['Bob', '28', 'UK'],
-    ];
-  
     return (
       <View>
-       <Text>Hiiiiii There</Text>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <ListItem bottomDivider>
+              <Icon name="person" />
+              <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>Age: {item.age}</ListItem.Subtitle>
+                <ListItem.Subtitle>Country: {item.country}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          )}
+        />
       </View>
     );
   };
-
 
 export default ViewList;
