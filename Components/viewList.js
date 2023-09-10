a027e18ff9e65a6aeacb9978a1fff0ea087abb06
DESKTOP-HD94HJ1\Manoj <om.bhaisora@gmail.com>
2023-09-10 15:48:27 +0530

    list

--- Components/viewList.js
+++ Components/viewList.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import { View, FlatList, Text } from 'react-native';
-import { ListItem, Icon } from 'react-native-elements';
+import * as React from 'react';
+import { FlatList } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
-
-const data = [
-    { id: '1', name: 'John', age: '30', country: 'USA' },
-    { id: '2', name: 'Alice', age: '25', country: 'Canada' },
-    { id: '3', name: 'Bob', age: '28', country: 'UK' },
-    // Add more data as needed
-  ];
+const optionsPerPage = [2, 3, 4];
 
 const ViewList = () => {
+    const data = [...Array(20).keys()];
+    const [page, setPage] = React.useState(0);
+    const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
+
+    React.useEffect(() => {
+      setPage(0);
+    }, [itemsPerPage]);
+
+    const _renderItem = ({ item }) => (
+        <DataTable.Row>
+          <DataTable.Cell>{item}</DataTable.Cell>
+          <DataTable.Cell numeric>159</DataTable.Cell>
+          <DataTable.Cell numeric>6.0</DataTable.Cell>
+          <DataTable.Cell numeric>450</DataTable.Cell>
+        </DataTable.Row>
+      );
+
     return (
-      <View>
-        <FlatList
-          data={data}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <ListItem bottomDivider>
-              <Icon name="person" />
-              <ListItem.Content>
-                <ListItem.Title>{item.name}</ListItem.Title>
-                <ListItem.Subtitle>Age: {item.age}</ListItem.Subtitle>
-                <ListItem.Subtitle>Country: {item.country}</ListItem.Subtitle>
-              </ListItem.Content>
-            </ListItem>
-          )}
-        />
-      </View>
+    //   <View>
+    //     <FlatList
+    //       data={data}
+    //       keyExtractor={(item) => item.id}
+    //       renderItem={({ item }) => (
+    //         <ListItem bottomDivider>
+    //           <Icon name="person" />
+    //           <ListItem.Content>
+    //             <ListItem.Title>{item.name}</ListItem.Title>
+    //             <ListItem.Subtitle>Age: {item.age}</ListItem.Subtitle>
+    //             <ListItem.Subtitle>Country: {item.country}</ListItem.Subtitle>
+    //           </ListItem.Content>
+    //         </ListItem>
+    //       )}
+    //     />
+    //   </View>
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Name</DataTable.Title>
+        <DataTable.Title numeric>Cost(Incoming)</DataTable.Title>
+        <DataTable.Title numeric>Cost(Outgoing)</DataTable.Title>
+        <DataTable.Title numeric>Cost(Outgoing)</DataTable.Title>
+      </DataTable.Header>
+
+      <FlatList data={data} renderItem={_renderItem} />
+    </DataTable>
     );
-  };
+};
 
 export default ViewList;
